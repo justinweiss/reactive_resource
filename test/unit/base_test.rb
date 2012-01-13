770635336d1939c148f08ead84c1b453e2246097
Flavio Castelli <fcastelli@suse.com>
2012-01-13 17:13:41 +0100

    Fixes required to work with ActiveResouce > 3.0.0
    * explicitly add active_support to the dependecies list
    * update Gemfile.lock via bundler
    * fix broken code inside of ReactiveResource::Base
    * fix broken unit tests

--- test/unit/base_test.rb
+++ test/unit/base_test.rb
@@ -19,13 +19,13 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
       assert_equal ReactiveResource::Lawyer, @object.class.associations.detect {|assoc| assoc.attribute == :lawyer }.associated_class
     end
   end
-  
+
   context "A resource that inherits from ReactiveResource::Base" do
-    
+
     setup do
       @object = ReactiveResource::Lawyer.new
     end
-    
+
     should "hit the Avvo API with the correct URL when saved" do
       stub_request(:post, "https://api.avvo.com/api/1/lawyers.json")
       @object.save
@@ -33,14 +33,16 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
     end
 
     should "hit the Avvo API with the correct URL when retrieved" do 
-      stub_request(:get, "https://api.avvo.com/api/1/lawyers/1.json").to_return(:body => {:id => '1'}.to_json)
+      stub_request(:get, "https://api.avvo.com/api/1/lawyers/1.json")\
+        .to_return(:body => {:lawyer => {:id => '1'}}.to_json)
       ReactiveResource::Lawyer.find(1)
       assert_requested(:get, "https://api.avvo.com/api/1/lawyers/1.json")      
     end
 
     context "with a has_many relationship to another object" do
       should "hit the associated object's URL with the correct parameters when requested" do
-        stub_request(:get, "https://api.avvo.com/api/1/lawyers/1/addresses.json")
+        stub_request(:get, "https://api.avvo.com/api/1/lawyers/1/addresses.json")\
+          .to_return(:body => [{:address => {:id => 1}}].to_json)
         @object.id = 1
         @object.addresses
         assert_requested(:get, "https://api.avvo.com/api/1/lawyers/1/addresses.json")
@@ -49,7 +51,8 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
 
     context "with a has_one relationship to another object" do
       should "hit the associated object's URL with the correct parameters when requested" do
-        stub_request(:get, "https://api.avvo.com/api/1/lawyers/1/headshot.json").to_return(:body => {:headshot_url => "blah"}.to_json)
+        stub_request(:get, "https://api.avvo.com/api/1/lawyers/1/headshot.json")\
+          .to_return(:body => {:headhshot => {:headshot_url => "blah"}}.to_json)
         @object.id = 1
         @object.headshot
         assert_requested(:get, "https://api.avvo.com/api/1/lawyers/1/headshot.json")
@@ -60,21 +63,23 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
       setup do 
         @object = ReactiveResource::Address.new(:lawyer_id => 2)
       end
-      
+
       should "hit the Avvo API with the correct URL when saved" do
         stub_request(:post, "https://api.avvo.com/api/1/lawyers/2/addresses.json")
         @object.save
         assert_requested(:post, "https://api.avvo.com/api/1/lawyers/2/addresses.json")
       end
-      
+
       should "hit the Avvo API with the correct URL when retrieved" do 
-        stub_request(:get, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json").to_return(:body => {:id => '3'}.to_json)
+        stub_request(:get, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json")\
+          .to_return(:body => {:address => {:id => '3'}}.to_json)
         ReactiveResource::Address.find(3, :params => {:lawyer_id => 2})
-        assert_requested(:get, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json")      
+        assert_requested(:get, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json")
       end
 
       should "hit the Avvo API with the correct URL when updated" do 
-        stub_request(:get, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json").to_return(:body => {:id => '3'}.to_json)
+        stub_request(:get, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json")\
+          .to_return(:body => {:address => {:id => '3'}}.to_json)
         license = ReactiveResource::Address.find(3, :params => {:lawyer_id => 2})
         assert_requested(:get, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json")  
         stub_request(:put, "https://api.avvo.com/api/1/lawyers/2/addresses/3.json")
@@ -83,7 +88,8 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
       end
 
       should "set the prefix parameters correctly when saved" do
-        stub_request(:post, "https://api.avvo.com/api/1/lawyers/2/addresses.json").to_return(:body => {:id => '2'}.to_json)
+        stub_request(:post, "https://api.avvo.com/api/1/lawyers/2/addresses.json")\
+          .to_return(:body => {:address => {:id => '2'}}.to_json)
         @object.save
         assert_requested(:post, "https://api.avvo.com/api/1/lawyers/2/addresses.json")
 
@@ -112,7 +118,8 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
         @object.doctor_id = 2
         @object.address_id = 3
         assert_equal 3, @object.address_id
-        stub_request(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3.json").to_return(:body => {:id => '3'}.to_json)
+        stub_request(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3.json")\
+          .to_return(:body => {:address => {:id => '3'}}.to_json)
         @object.address
         @object.address
         assert_requested(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3.json", :times => 1)
@@ -125,7 +132,8 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
       end
 
       should "hit the Avvo API with the correct URL when updated" do 
-        stub_request(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3/phones/4.json").to_return(:body => {:id => '4'}.to_json)
+        stub_request(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3/phones/4.json")\
+          .to_return(:body => {:address => {:id => '4'}}.to_json)
         phone = ReactiveResource::Phone.find(4, :params => {:doctor_id => 2, :address_id => 3})
         assert_requested(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3/phones/4.json")
 
@@ -141,9 +149,10 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
 
         assert_equal "/api/1/doctors/2/addresses/3/phones/4.json", @object.send(:element_path)
       end
-      
+
       should "hit the Avvo API with the correct URL when retrieved" do 
-        stub_request(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3/phones/4.json").to_return(:body => {:id => '4'}.to_json)
+        stub_request(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3/phones/4.json")\
+          .to_return(:body => {:phone => {:id => '4'}}.to_json)
         ReactiveResource::Phone.find(4, :params => {:doctor_id => 2, :address_id => 3})
         assert_requested(:get, "https://api.avvo.com/api/1/doctors/2/addresses/3/phones/4.json")      
       end
@@ -153,15 +162,17 @@ class ReactiveResource::BaseTest < Test::Unit::TestCase
 
   context "A resource without an extension" do
     should "hit the correct urls" do
-      stub_request(:get, "https://api.avvo.com/api/1/no_extensions")
+      stub_request(:get, "https://api.avvo.com/api/1/no_extensions")\
+        .to_return(:body => [].to_json)
       @object = ReactiveResource::NoExtension.find(:all)
       assert_requested(:get, "https://api.avvo.com/api/1/no_extensions")
 
-      stub_request(:put, "https://api.avvo.com/api/1/no_extensions/1")
+      stub_request(:post, "https://api.avvo.com/api/1/no_extensions")
       @object = ReactiveResource::NoExtension.new(:id => 1).save
-      assert_requested(:put, "https://api.avvo.com/api/1/no_extensions/1")
+      assert_requested(:post, "https://api.avvo.com/api/1/no_extensions")
 
-      stub_request(:get, "https://api.avvo.com/api/1/no_extensions/test")
+      stub_request(:get, "https://api.avvo.com/api/1/no_extensions/test")\
+        .to_return(:body => {:test => {:id => 1}}.to_json)
       @object = ReactiveResource::NoExtension.get(:test)
       assert_requested(:get, "https://api.avvo.com/api/1/no_extensions/test")
     end
